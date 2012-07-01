33958ba399fd48811869620cb2f5053ac89f37b3
kraki5525 <kraki5525@gmail.com>
2012-06-30 23:41:46 -0400

    Updating bootstrapper for static content

--- src/Testr/TestrBootstrapper.cs
+++ src/Testr/TestrBootstrapper.cs
@@ -2,11 +2,18 @@
 
 using Nancy;
 using Nancy.Bootstrappers.Autofac;
+using Nancy.Conventions;
 
 namespace Testr
 {
     public class TestrBootstrapper : DefaultNancyBootstrapper //AutofacNancyBootstrapper
     {
+        protected override void ConfigureConventions(Nancy.Conventions.NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
 
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("Content"));
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("Scripts"));
+        }
     }
 }
